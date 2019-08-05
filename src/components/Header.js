b7a55afa9c5c7f954d80c8b25b53a77787976682
Randy Burgess <wrburgess@gmail.com>
2019-08-05 16:59:55 -0500

    adding sass and docs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import "./Header.scss";
 
 class Header extends React.Component {
   render() {
     return (
-      <div>
+      <header>
         This is the Header
-      </div>
+        <div className="foo">FOO BAR</div>
+      </header>
     );
   }
 }
