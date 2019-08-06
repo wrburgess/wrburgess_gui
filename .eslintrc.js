5c004bfca32ccdfc3ad1a18dc49f66c60a3c4dab
Randy Burgess <wrburgess@gmail.com>
2019-08-06 17:32:05 -0500

    clean up manifest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: [
-    'plugin:prettier/recommended',
-    'eslint:recommended',
-    'plugin:react/recommended',
-  ],
-  plugins: ['react'],
   env: {
     es6: true,
     browser: true,
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
+    ecmaVersion: 6,
+    sourceType: 'module',
   },
+  plugins: [
+    'react'
+  ],
   rules: {
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
