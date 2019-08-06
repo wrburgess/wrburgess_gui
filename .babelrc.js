5713ce2d859d99282cf8820b1d4412424d01bc8a
Randy Burgess <wrburgess@gmail.com>
2019-08-06 17:32:40 -0500

    clean up configs

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: ['env', 'react'],
+  presets: [
+    'env', 'react'
+  ],
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  trailingComma: 'es5',
-  tabWidth: 2,
   semi: true,
   singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
 };
